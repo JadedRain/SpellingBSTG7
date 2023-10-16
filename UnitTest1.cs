14dec4578f33a27c56078d415c4bc633a81ce675
Logan Douglas <logan.douglas@students.snow.edu>
2023-10-16 09:22:41 -0600

    Tree Stuff

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -13,3 +13,60 @@ public class Tests
         Assert.Pass();
     }
 }
+
+public interface ISortedSet<T> : IComparable<T>
+{
+    bool add(T value);
+    T remove(T value);
+    T find(T value);
+}
+
+public interface ITraversable<T>
+{
+    IEnumerable<T> PreOrder();
+    IEnumerable<T> InOrder();
+    IEnumerable<T> PostOrder();
+}
+
+public class Tree<T> : ISortedSet<T>, ITraversable<T>
+{
+    TreeNode? root = null;
+    int size = 0;
+
+    class TreeNode<T>
+    {
+        public T Value;
+        public Tree<T> LeftChild;
+        public Tree<T> RightChild;
+        TreeNode(T value, Tree<T> leftChild, Tree<T> rightChild)
+        {
+            Value = value;
+            LeftChild = leftChild;
+            RightChild = rightChild;
+        }
+    }
+
+    public bool add(T value)
+    {
+
+    }
+
+    public int size(TreeNode<T> node)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+        return 1 + size(node.LeftChild) + size(node.RightChild);
+    }
+
+    public T remove(T value)
+    {
+
+    }
+
+    public T find(T value)
+    {
+
+    }
+}
